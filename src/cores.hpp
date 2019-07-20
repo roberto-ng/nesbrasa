f59e98289161554371f67da437bee9a7b2b8267c
Roberto Nazareth <nazarethguedesroberto@gmail.com>
2019-07-20 04:34:54 -0300

    tabela de cores

---
+++ src/cores.hpp
@@ -0,0 +1,34 @@
+/* cores.hpp
+ *
+ * Copyright 2019 Roberto Nazareth <nazarethroberto97@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <array>
+
+#include "tipos_numeros.hpp"
+
+namespace nesbrasa::nucleo::cores
+{
+    using std::array;
+    using namespace nesbrasa::tipos;
+
+    using cor_rgb = array<byte, 3>;
+
+    cor_rgb buscar_cor_rgb(byte valor);
+    extern array<cor_rgb, 0x40> tabela_rgb;
+}
